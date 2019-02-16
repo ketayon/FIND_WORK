3c4cb9c8f44d1e1352c44f589bf1dccd267678a8
ketayon <ketayon@gmail.com>
2019-02-16 14:14:14 +0200

    changed production.py

--- find_work/settings/production.py
+++ find_work/settings/production.py
@@ -34,7 +34,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['findwork-app.herokuapp.com']
 
 
 # Application definition
